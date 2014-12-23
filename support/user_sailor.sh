56409c12841c6c5fe747ac345e581c269077464c
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-12-23 06:35:06 -0600

    latest node

--- support/user_sailor.sh
+++ support/user_sailor.sh
@@ -3,6 +3,6 @@
 git clone https://github.com/creationix/nvm.git $HOME/.nvm
 cat /docker-build/support/bashrc_append_sailor.txt >> $HOME/.bashrc
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.32
-nvm alias default 0.10.32
+nvm install v0.10.34
+nvm alias default 0.10.34
 npm update -g
